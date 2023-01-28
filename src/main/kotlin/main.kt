655ccf6b081e55dadc3c367a4160bda48603c7cc
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-28 17:15:34 -0500

    ejercicio1_calcular_perimetro_y_area_dado_su_radio

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
+const val PI=3.1416
 fun main(){
 
+println("Programa que calcule el área y perímetro de un circulo")
+println("Dame el Radio:")
+   val radio = readLine()!!.toInt()
+    val perimetro = radio * 2 * PI
+    val area = radio * 2 * PI
+
+    println("el area es $area y el perimetro $perimetro ")
 }
