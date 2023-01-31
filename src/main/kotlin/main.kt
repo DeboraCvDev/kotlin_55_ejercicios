b83a62c11cb2ac59fb3af0d024f51c22f895930a
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-31 15:21:43 -0500

    ejercicio2_programa_para_calcular_el_cociente_y_residuo_dado_dos_numero_enteros

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
 fun main(){
+    println("Programa para calcular el residuo y cociente de un numeros entre otro ")
+    println("Dame el divisor")
+    val primerNumero: Int = readLine()!!.toInt()
 
+
+    println("Dame el dividendo")
+    val segundoNumero: Int = readLine()!!.toInt()
+
+    val calcularElCociente: Int = primerNumero/segundoNumero
+
+    val  calcularElResiduo: Int = primerNumero%segundoNumero
+
+    println("El cociente es igual a :$calcularElCociente y el residuo es igual a :$calcularElResiduo")
+    
 }
