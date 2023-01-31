d0d29f7fa53965e136cc67af201bc82c524036dd
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-31 16:08:37 -0500

    ejercicio4_programa_que_realice_la_suma_de_dos_numero_enteros

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
+println("Programa para calcular la suma de dos numeros enteros")
+    println("Dame tu primer numero")
+    val primerNumero: Int = readLine()!!.toInt()
 
+    println("Dame tu segundo numero")
+    val segundoNumero: Int = readLine()!!.toInt()
+
+    val calcularSuma: Int = primerNumero+segundoNumero
+
+    println("El resultado a la suma es: $calcularSuma")
 }
