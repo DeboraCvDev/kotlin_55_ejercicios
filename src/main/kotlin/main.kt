6e652d6f07d9d10df9b52b255eda8fb742cda529
DeboraCvDev <deboracornelio14@gmail.com>
2023-01-31 15:44:08 -0500

    ejercicio3_programa_para calcular_el_area_y_perimetro_de_un_pentagono

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,17 @@
-fun main(){
+fun main() {
+    println("Programa para calcular el area y perimetro de un pentágono")
+    println("Dame el lado")
+    val lado: Int = readLine()!!.toInt()
 
+    println("Dame la apotema")
+    val apotema: Int = readLine()!!.toInt()
+
+
+
+
+    val calcularArea = (lado * 5 * apotema/2)
+
+    val calcularPerimetro: Int = lado*5
+
+    println("El area del pentágono es: $calcularArea y el perímetro es: $calcularPerimetro")
 }
