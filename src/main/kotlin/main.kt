1ade22e82aba846d561a38768411a46947a7d8ab
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-02 15:28:52 -0500

    ejercicio5_programa_que_calcule_el_área_y_perímetro_de_un_rectángulo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,18 @@
 fun main(){
+println("Programa que calcule el área y perímetro de un rectángulo")
+    println("Dame la base")
+ val base:Int= readLine()!!.toInt()
+
+    println("Dame el perimetro")
+ val perimetro:Int= readLine()!!.toInt()
+
+
+
+    val calcularArea = base*perimetro
+
+    val calcularPerimetro = 2*base+perimetro
+
+
+    println("El área del rectángulo es: $calcularArea y el perímetro es: $calcularPerimetro")
 
 }
