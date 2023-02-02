18dd5be57b71efc76b1452faf10da8fc2c803600
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-02 15:18:33 -0500

    ejercicio5_programa_que_calcule_el_cuadrado_de_un_numero

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main(){
+    println("Programa que calcule el cuadrado de un numero")
+    println("Dame el numero")
+    val numero: Int= readLine()!!.toInt()
 
+
+    val calcularElCuadradoDeUnNumero: Int = numero*numero
+    println("El cuadrado de $numero es $calcularElCuadradoDeUnNumero")
 }
