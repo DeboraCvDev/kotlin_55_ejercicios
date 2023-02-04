4c135a694bde74f865bd631f39330925812ae3cc
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 17:13:00 -0500

    ejercicio12_Programa_que_indique_si_un_número_es_múltiplo_de_6

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
-
+println("Programa que indique si un número es múltiplo de 6")
+    println("Dame un numero:")
+    val primerNumero: Int = readLine()!!.toInt()
+    if (primerNumero%6 ==0){
+        println("El $primerNumero es multiplo de 6")
+    }
+    if (primerNumero%6!=0){
+        println("El $primerNumero es multiplo de 6")
+    }
 }
