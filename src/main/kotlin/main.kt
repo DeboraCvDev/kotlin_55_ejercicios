96b0f23b624f9daefcd7c0054a920dfd8e1d5551
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 16:05:29 -0500

    ejercicio9_programa_que_calcule_el_porcentaje

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
-fun main(){
-
+fun main() {
+    println("Programa que calcule el porcentaje:")
+    println("Introduce la cantida:")
+    val cantidad: Int = readLine()!!.toInt()
+    println("Introduce porcentaje:")
+    val porcentaje: Int = readLine()!!.toInt()
+    val calcularPorcentaje: Int = cantidad*porcentaje
+    println("El porcentaje es $calcularPorcentaje")
 }
