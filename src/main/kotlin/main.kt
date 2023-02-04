2cfacbb946ee64521216ca8fd0596bcefea00d71
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 17:22:58 -0500

    ejercicio13_programa_que_indique_si_un_numero_es_par_o_impar

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
-
+println("Programa que indica si un numero es par o impar")
+    println("Dame un numero:")
+    val primerNumero: Int = readLine()!!.toInt()
+    if (primerNumero%2==0){
+        println("El numero $primerNumero es par")
+    }
+    else{
+        println("El numero $primerNumero no es par")
+    }
 }
