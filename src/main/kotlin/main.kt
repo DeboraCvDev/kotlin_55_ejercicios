5f6cb9a45244d7a7ac65d281bdc8716ccb4b7788
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 16:48:46 -0500

    ejercicio11_Programa_que_indique_si_un_numero_es_divisible entre_14

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main(){
-
+println("Programa que indique si un numero es divisible entre 14")
+    println("Dame el numero:")
+    val primerNumero: Int = readLine()!!.toInt()
+    if (primerNumero/14==0){
+        println("El numero $primerNumero si es divisible entre 14")
+    }
+    if (primerNumero/14!=0){
+        println("El numero $primerNumero no es divisble entre 14")
+    }
 }
