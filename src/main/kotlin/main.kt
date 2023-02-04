7461040ad3d4f37ab3cd0511d94f1972d8cbba38
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 15:26:20 -0500

    ejercicio8_programa_que_calcule_la_hipotenusa_de_un_triangulo_rectangulo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
-fun main(){
+import kotlin.math.*
 
+fun main() {
+    println("Programa que calcule la hipotenusa de un triangulo rectangulo")
+    println("¿Cuanto mide el primer cateto?")
+    val primerCateto: Int = readLine()!!.toInt()
+    println("¿Cuanto mide el segundo cateto?")
+    val segundoCateto: Int = readLine()!!.toInt()
+    val calcularHipotenusa: Int =
+        sqrt((primerCateto.toDouble() * primerCateto) * (segundoCateto * segundoCateto)).toInt()
+    println(La hipotenusa es:"$calcularHipotenusa")
 }
