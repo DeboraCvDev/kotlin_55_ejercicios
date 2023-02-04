a172511ae236da7f1ff9f36eb33f7d04b42e474a
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 17:42:42 -0500

    ejercicio15_programa_que_indique_si_un_número_es_divisible_entre_dos_y_cinco_(a_la_vez)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
-fun main(){
-
+fun main() {
+    println("programa que indique si un número es divisible entre dos y cinco (a la vez)")
+    println("Dame el numero:")
+    val primerNumero: Int = readLine()!!.toInt()
+    if (primerNumero % 5 == 0 && primerNumero % 2 != 0) {
+        println("El numero $primerNumero es divisible entre 2 y 5")
+    } else {
+        println("El numero $primerNumero no es divisible entre 2 y 5")
+    }
 }
