19482a9d4da6135cf820696b3f77f3599343c2a9
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 17:36:14 -0500

    ejercicio14_programa_que_indique_si_la_suma_de_dos_valores_es_positiva_negativa_o_cero

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
-fun main(){
-
+fun main() {
+    println("programa que indique si la suma de dos valores es positiva, negativa o cero")
+    println("Dame los numeros:")
+    val primerNumero: Int = readLine()!!.toInt()
+    val segundoNumero: Int = readLine()!!.toInt()
+    val suma: Int =primerNumero+segundoNumero
+    if (suma>0){
+        println("La suma $suma es positiva")
+    }
+    if (suma<0){
+        println("La suma $suma es negativa")
+    }
+    if (suma==0){
+        println("La suma $suma es igual a 0")
+    }
 }
