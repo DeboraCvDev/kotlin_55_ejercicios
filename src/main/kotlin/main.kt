3c45f4edc74c8c13c76d82f0be0bfccbfdfb854a
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 16:35:10 -0500

    ejercicio10_programa_que_indique_el_mayor_numero

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
-fun main(){
-
+fun main() {
+    println("Programa que indique el mayor numero:")
+    println("Dame los numeros")
+    val primerNumero: Int = readLine()!!.toInt()
+    val segundoNumero: Int = readLine()!!.toInt()
+    val tercerNumero: Int = readLine()!!.toInt()
+    val cuartoNumero: Int = readLine()!!.toInt()
+    if (primerNumero > segundoNumero && primerNumero > tercerNumero && primerNumero > cuartoNumero) {
+        println("El mayor numero es:$primerNumero")
+    }
+    if (segundoNumero > primerNumero && segundoNumero > tercerNumero && segundoNumero > cuartoNumero) {
+        println("El mayor numero es:$segundoNumero")
+    }
+    if (tercerNumero > primerNumero && tercerNumero > segundoNumero && tercerNumero > cuartoNumero) {
+        println("El mayor numero es: $tercerNumero")
+    }
+    if (cuartoNumero > primerNumero && cuartoNumero > segundoNumero && cuartoNumero > tercerNumero) {
+        println("El mayor numero es: $cuartoNumero")
+    }
 }
