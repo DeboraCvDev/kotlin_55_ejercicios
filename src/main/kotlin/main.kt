c21509323fcf34b3dd90505f131e2286ace1a7c0
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-04 14:57:51 -0500

    ejercicio7_programa_que_invierta_todo_lo_que_que_lee

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
-fun main(){
-
+fun main() {
+    println("Programa para informar modelo y nombre de automoviles:")
+    println("Dame la marca del auto:")
+    val marcaDelAuto: String = readln().reversed()
+    println("Dame el modelo del auto:")
+    val modeloDelAuto: String = readln().reversed()
+    println("La marca es: $marcaDelAuto y el modelo es: $modeloDelAuto")
 }
