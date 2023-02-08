0314af7ea761196acd055adc938061ce8e11f20d
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-08 10:32:16 -0500

    ejercicio16_programa_que_indique_peso_ideal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
-fun main(){
-
+fun main() {
+    println("Peso ideal")
+    println("Dame tu estatura y tu peso")
+    val estatura: Int = readLine()!!.toInt()
+    val peso: Int = readLine()!!.toInt()
+    val pesoIdeal: Int =estatura-100
+    if (peso>0 && estatura>0){
+        if (peso>pesoIdeal){
+            println("Sobre Peso!")
+        }
+    }
+    if (peso==pesoIdeal){
+        println("Peso ideal!")
+    }
+    if (peso<pesoIdeal){
+        println("Peso Bajo!")
+    } else {
+        println("Error!")
+    }
 }
