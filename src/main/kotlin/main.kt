9dfc2f058757a16d1a278320a587bfbc79e088fd
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-08 10:55:49 -0500

    ejercicio17_programa_que_calcule_el_cambio_que_debe_darse_a_un_cliente

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
-fun main(){
-
+fun main() {
+    println("Programa que calcule el cambio que debe darse a un cliente")
+    println("Dinero entregado por el cliente:")
+    val dinero: Int = readLine()!!.toInt()
+    println("Costo del producto:")
+    val costo: Int = readLine()!!.toInt()
+    if (dinero>=costo){
+        val m: Int = dinero-costo
+        println("El cambio es:$m")
+    }else{
+        val f: Int = costo-dinero
+      println("Te falta $f, no puedes hacer la compra")
+    }
 }
