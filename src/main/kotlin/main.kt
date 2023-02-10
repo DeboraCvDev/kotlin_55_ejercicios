8a255bdd7b716dabd26ef52e6df0e9b9633d7a96
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-10 11:30:14 -0500

    ejercicio18_programa_para_determinar_si_un_angulo_es_agudo_u_obtuso

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
-fun main(){
+fun main() {
+    println("Programa para determinar si un angulo es agudo u obtuso")
+    println("Dame un angulo:")
+    val primerAngulo: Int = readLine()!!.toInt()
+    if (primerAngulo >= 0) {
+        if (primerAngulo < 90) {
+            println("Angulo agudo")
+        }
+    }
+    if (primerAngulo > 90) {
+        println("Angulo obtuso")
+    }
 
 }
