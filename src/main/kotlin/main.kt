21598174360b3ee610081d232f9d3bef38d52856
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-10 11:44:26 -0500

    ejercicio19_programa_que_despliegue_el_día_de_la_semana_dado_un_número_de_día

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,28 @@
-fun main(){
-
+fun main() {
+    println("Programa que despliegue el día de la semana dado un número de día")
+    println("Dame el día:")
+    val dia: Int = readLine()!!.toInt()
+    if (dia == 1) {
+        println("Lunes")
+    }
+    if (dia == 2) {
+        println("Martes")
+    }
+    if (dia == 3) {
+        println("Miercoles")
+    }
+    if (dia == 4) {
+        println("Jueves")
+    }
+    if (dia == 5) {
+        println("Viernes")
+    }
+    if (dia == 6) {
+        println("Sabado")
+    }
+    if (dia == 7) {
+        println("Domingo")
+    }else{
+        println("Error!")
+    }
 }
