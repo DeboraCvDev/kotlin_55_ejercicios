916f1967aa14e69cc7199637afeb127c5dc415d0
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-11 18:12:52 -0500

    ejercicio21n_programa_que_calcule_el_área_de_un_cuadrado,_un_rectángulo_y_un_triángulo

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,32 @@
-fun main(){
+fun main() {
+    println("Programa que calcule areas de figuras")
+    println("Opciones:")
+    println("1.Cuadrado")
+    println("2.Rectangulo")
+    println("3.Triangulo")
+    println("4.Salir")
+    val valorIngresado:Int = readLine()!!.toInt()
+    println("Ingrese la primera medida:")
+    val primeraMedida: Int = readLine()!!.toInt()
+    println("Ingrese la segunda medida:")
+    val segundaMedida: Int = readLine()!!.toInt()
+    when(valorIngresado){
+        1->{
+            println("El área del cuadrado es: $primeraMedida x $segundaMedida :")
+            println("Es-->${primeraMedida*segundaMedida} ")
+        }
+        2->{
+            println("El área del rectangulo es: $primeraMedida x $segundaMedida :")
+            println("Es-->${primeraMedida*segundaMedida} ")
+        }
+        3->{
+            println("El área del triangulo es: $primeraMedida x $segundaMedida / 2:")
+            println("Es-->${primeraMedida*segundaMedida/2} ")
+        }
+       4->{
+           println("Fin del programa")
 
+       }
+
+    }
 }
