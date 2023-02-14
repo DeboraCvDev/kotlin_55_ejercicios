7a4caf98ee5d6c1b666af0d2333f4ab12c92021e
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-14 16:06:15 -0500

    ejercicio24_programa_que_sume_la_serie_3,6,9_hasta_99_y_despliegue_su_resultado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
-fun main(){
+fun main() {
+    println("Programa que suma la serie del 3 al 99 de 3 en 3")
+    var I = 3
+    var A = 0
+    while (I <= 99){
+        println(I)
+          A = A + I
+       I = I + 3
+        }
+        println("La suma de la serie es:$A ")
 
 }
