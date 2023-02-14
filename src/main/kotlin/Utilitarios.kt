55a9795e9ca84f1a97c3e23aa788df093358487f
DeboraCvDev <deboracornelio14@gmail.com>
2023-02-14 15:13:38 -0500

    ejercicio22_programa_que_calcule_el_más_grande,_el_más_pequeño_y_la_media_de_N_números._

---
+++ src/main/kotlin/Utilitarios.kt
@@ -0,0 +1,31 @@
+fun calcularGrandePequeñoMedio(cantidadDeNumeros: Int){
+    var i = 0
+    var o = 0
+    var p: Int = 0
+    var g: Int = 0
+    var m: Int = 0
+    var a: Int = 0
+    if (cantidadDeNumeros > 0){
+    do {
+        println("Ingrese un numero:")
+        val numero = readLine()!!.toInt()
+        if (i == 0){
+            p = numero
+            g = numero
+        }
+        if (numero < p){
+            p = numero
+        }
+        if (numero > g){
+            g = numero
+        }
+        i = i + 1
+        a = a + numero
+    }while (i < cantidadDeNumeros)
+
+    m = a / cantidadDeNumeros
+        println("Mayor $g Menor $p Medio $m")
+    }else {
+        print("Error..")
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
-fun main(){
-
+fun main() {
+    println("Calcule el mas grande, el mas pequeño y la media de N numeros.")
+    println("Ingrese la cantidad de numeros:")
+    var cantidadDeNumeros: Int = readLine()!!.toInt()
+    calcularGrandePequeñoMedio(cantidadDeNumeros)
 }
