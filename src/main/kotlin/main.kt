079aff66b3f83c619de0a8f037daf1322cbbfc82
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-15 14:38:17 -0500

    ejercicio26Escribir_un_programa_que_sume_la_serie_3,6,9_hasta_99_y_despliegue_su_resultado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
-fun main(){
+fun main() {
+    println("Escribir un programa que sume la serie 3,6,9 hasta 99 y despliegue su resultado")
 
+    var I = 3
+    var A = 0
+    while (I <= 99){
+        println("$I")
+        A += I
+        I += 3
+    }
+    println("La suma de los numeros es: $A")
 }
