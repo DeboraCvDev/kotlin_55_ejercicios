c7ddf0ecc0de1f600046506aac1783f9eba4e1c9
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-15 14:43:23 -0500

    ejercicio27_Escribir_un_programa_que_imprima_los_impares_del_7_al_51

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
-fun main(){
+fun main() {
+    println("Escribir un programa que imprima los impares del 7 al 51")
 
+    var I = 7
+    while (I <= 51) {
+        println("$I")
+        I = I + 2
+    }
 }
