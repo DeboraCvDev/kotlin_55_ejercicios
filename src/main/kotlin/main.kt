7a4b3e3ba4dea47193fe0ed2e281f004d76f0d21
DeboraCvDev <deboracornelio14@gmail.com>
2023-03-15 15:00:19 -0500

    Ejercicio28_Escribir_un_programa_que_verifica_si_un_dato_ingresado_por_teclado_corresponde_a_la_contraseña_252525

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
-fun main(){
-
+fun main() {
+    println("Escribir un programa que verifica si un dato ingresado por teclado corresponde a la contraseña 252525")
+    println("Ingresa la cantraseña: ")
+    val contraseña: Int = readLine()!!.toInt()
+    if (contraseña == 252525){
+       println("La contraseña es correcta")
+    }
+   else{
+       println("La contraseña es Incorrecta! ")
+    }
 }
